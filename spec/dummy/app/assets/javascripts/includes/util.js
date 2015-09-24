2f27dc0f062556295bb6912812bdc34ac8f26b80
Diego Felipe <dev6.seucondominio@gmail.com>
2015-09-24 09:49:57 -0300

    simplificação .jade

--- spec/dummy/app/assets/javascripts/includes/util.js
+++ spec/dummy/app/assets/javascripts/includes/util.js
@@ -1,14 +1,14 @@
-// This is a mock Util object
-// Used for testing server-side inclusion of Javascripts
-;(function(global){
+// // This is a mock Util object
+// // Used for testing server-side inclusion of Javascripts
+// ;(function(global){
 
-  var Util = {
-    ISODate: function(){
-      var d = new Date()
-      return d.toISOString()
-    }
-  };
-  // attach to global object to make it available inside jade templates with node.js
-  global.Util = Util;
+//   var Util = {
+//     ISODate: function(){
+//       var d = new Date()
+//       return d.toISOString()
+//     }
+//   };
+//   // attach to global object to make it available inside jade templates with node.js
+//   global.Util = Util;
 
-})(this);
+// })(this);
